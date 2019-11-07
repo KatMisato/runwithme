5190042cdb55a03df444f0b382afad1a3e6bd598
Mariia Bazhina <kat.misato@gmail.com>
2019-11-07 22:38:02 +0300

    initial commit

---
+++ install_packages.bat
@@ -0,0 +1,29 @@
+call npm install
+call npm install lite-server --save-dev
+call npm install bootstrap@4.0.0 --save
+call npm install jquery@3.3.1 popper.js@1.12.9 --save
+call npm install font-awesome --save
+call npm install bootstrap-social@5.1.1 --save
+call npm install --save-dev onchange@3.3.0 parallelshell@3.0.2
+call npm install --save-dev rimraf@2.6.2
+call npm -g install copyfiles@2.0.0
+call npm -g install imagemin-cli@3.0.0
+call npm install mdbootstrap --save-dev
+call npm install --save-dev usemin-cli@0.5.1 cssmin@0.4.3 uglifyjs@2.4.11 htmlmin@0.0.7
+call npm install -g grunt-cli@1.2.0
+call npm install grunt@1.0.2 --save-dev
+call npm install grunt-sass@2.1.0 --save-dev
+call npm install time-grunt@1.4.0 --save-dev
+call npm install jit-grunt@0.10.0 --save-dev
+call npm install grunt-contrib-watch@1.0.0 --save-dev
+call npm install grunt-browser-sync@2.2.0 --save-dev
+call npm install grunt-contrib-copy@1.0.0 --save-dev
+call npm install grunt-contrib-clean@1.1.0 --save-dev
+call npm install grunt-contrib-imagemin@2.0.1 --save-dev
+call npm install grunt-contrib-concat@1.0.1 --save-dev
+call npm install grunt-contrib-cssmin@2.2.1 --save-dev
+call npm install grunt-contrib-htmlmin@2.4.0 --save-dev
+call npm install grunt-contrib-uglify@3.3.0 --save-dev
+call npm install grunt-filerev@2.3.1 --save-dev
+call npm install grunt-usemin@3.1.1 --save-dev
+call npm audit fix
